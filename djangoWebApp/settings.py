656a226cbb4acb29681dfa4137cb8ebcd54bc426
EthanSmithson <ethansmithson413@gmail.com>
2024-10-24 19:47:18 -0400

    corrections

--- djangoWebApp/settings.py
+++ djangoWebApp/settings.py
@@ -98,17 +98,18 @@ WSGI_APPLICATION = 'djangoWebApp.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-if 'DATABASE_URL' in os.environ:
+if DEVELOPMENT_MODE is True:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    print("Postgres URL not found, using sqlite instead")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
 
 
